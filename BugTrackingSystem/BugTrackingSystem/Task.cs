b7e52a8b37c6075b02189f8e2183709103d3a101
level-Miziak-Vladyslav <parado9921@gmail.com>
2018-12-10 19:39:49 +0200

    First Add

---
+++ BugTrackingSystem/BugTrackingSystem/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTrackingSystem
+{
+    class Task
+    {
+        public int Number;
+        public string Description;
+        public string Status;
+        public int Complexity;
+        public int Priority;
+    }
+}
